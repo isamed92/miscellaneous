820e8a766917ae6ceb508e531d8cc118759ed968
IsaiasPC <isamed92@gmail.com>
2019-01-06 01:02:05 -0300

    ngSwitch component and functionality done

---
+++ src/app/directives/resaltado.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appResaltado]'
+})
+export class ResaltadoDirective {
+
+  @Input('appResaltado') nuevoColor: string;
+
+  constructor(
+    private el: ElementRef
+  ) {
+    console.log('Directiva Llamada');
+    // el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseenter') mouseEntro() {
+    this.resaltar(this.nuevoColor || 'yellow');
+    // this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+  @HostListener('mouseleave') mouseSalio() {
+    this.resaltar(null);
+    // this.el.nativeElement.style.backgroundColor = null;
+  }
+
+  private resaltar(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
