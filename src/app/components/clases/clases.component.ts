9cd774896ab72abc233c78ce967950102c553490
IsaiasPC <isamed92@gmail.com>
2019-01-05 21:53:18 -0300

    ng class dev

---
+++ src/app/components/clases/clases.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+  alerta = 'alert-danger';
+
+  propiedades: object = {
+    danger: false
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
