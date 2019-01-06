9324da60952ab079642d17d77d1a3a81c8843bcd
IsaiasPC <isamed92@gmail.com>
2019-01-06 18:50:47 -0300

    add new usuario.routes.ts for children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { RUTAS_HIJAS } from './components/usuario/usuario.routes';
+
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     {
         path: 'usuario/:id',
         component: UsuarioComponent,
-        children: [
-            { path: 'nuevo', component: UsuarioNuevoComponent },
-            { path: 'editar', component: UsuarioEditarComponent },
-            { path: 'detalle', component: UsuarioDetalleComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
+        children: RUTAS_HIJAS
     },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
